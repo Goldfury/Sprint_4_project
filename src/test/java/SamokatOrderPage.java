af7b131105373b28c3c16711d7f05b6311b43134
Goldfure <a.konyskairov@gmail.com>
2024-08-18 03:21:51 +0500

    Внес изменения по комментариям, не смог все изменить так как появились вопросы к вашим комментариям, можете пожалуйста помочь?

--- src/test/java/SamokatOrderPage.java
+++ src/test/java/SamokatOrderPage.java
@@ -1,37 +1,26 @@
-import org.junit.Assert;
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.junit.runners.Parameterized;
 import org.openqa.selenium.By;
-import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.time.Duration;
 
-//@RunWith(Parameterized.class)
 public class SamokatOrderPage {
 
-    private WebDriver driver;
-    //локаторы для кнопок
-    private final By headerMakeOrderButton = By.xpath("//button[@class= 'Button_Button__ra12g']");
-    private final By bigMakeOrderButton = By.xpath("//div[@class='Home_FinishButton__1_cWm']/button");
+    private final WebDriver driver;
 
     // локаторы для полей персональных данных для заказа
     private final By nameField = By.xpath("//input[@placeholder='* Имя']");
     private final By surnameField = By.xpath("//input[@placeholder='* Фамилия']");
     private final By addressField = By.xpath("//input[contains(@placeholder, 'куда')]");
-        //Выпадающий список с метро
+    //Выпадающий список с метро
     private final By deliveryAddressField = By.xpath("//input[@placeholder = '* Станция метро']");
     private final By deliveryAddressFieldOption = By.cssSelector(".Order_SelectOption__82bhS.select-search__option");
     private final By phoneNumberField = By.xpath("//input[contains(@placeholder, 'курьер')]");
 
     //локаторы для второй части заказа
     private final By deliverDateField = By.xpath("//input[contains(@placeholder, '* Когда привезти самокат')]");
-        //Количество дней аренды
+    //Количество дней аренды
     private final By rentDaysField = By.className("Dropdown-placeholder");
     private final By rentDaysFieldDropdownOption = By.className("Dropdown-option");
     //Локатор для выбора цвета самоката
@@ -49,41 +38,24 @@ public class SamokatOrderPage {
     //Локатор кнопки для подтверждения заказа
     private final By confirmOrderButton = By.xpath("//button[@class='Button_Button__ra12g Button_Middle__1CSJM' and text()='Да']");
 
+    //Локатор для текста что заказ оформлен
+    private final By confirmOrderText = By.xpath("//div[@class = 'Order_ModalHeader__3FDaJ']");
 
-    public SamokatOrderPage(WebDriver driver){
+    public SamokatOrderPage(WebDriver driver) {
         this.driver = driver;
     }
-
-
-
-//    public SamokatOrderPage(WebDriver driver, String name, String surname, String address, String phoneNumber, String date, String comment) {
-//        this.name = name;
-//        this.surname = surname;
-//        this.address = address;
-//        this.phoneNumber = phoneNumber;
-//        this.date = date;
-//        this.comment = comment;
-//    }
-//
-//    // Тестовые данные
-//    @Parameterized.Parameters
-//    public static Object[][] getCredentials() {
-//        return new Object[][] {
-//                { "Акылбек", "Акылбек", "Астана", "87055463022", "21.07.2001", "Классный самокат" },
-//                { "Нурсултан", "Арманов", "Алматы", "87073332211", "21.04.1995", "что то новое"},
-//        };
-//    }
 
     private String name = "Акылбек";
     private String surname = "Акылбек";
     private String address = "Астана";
     private String phoneNumber = "87055463022";
     private String date = "21.07.2001";
-    private String comment = "Классный самокат" ;
+    private String comment = "Классный самокат";
 
     //заполнение формы заказа
-    public void fillSamokatOrderForm(){
-        driver.findElement(headerMakeOrderButton).click();
+    public void fillSamokatOrderForm() {
+        SamokatHomePageTest obj = new SamokatHomePageTest(driver);
+        obj.clickHeaderMakeOrderButton();
         driver.findElement(nameField).sendKeys(name);
         driver.findElement(surnameField).sendKeys(surname);
         driver.findElement(addressField).sendKeys(address);
@@ -106,19 +78,11 @@ public class SamokatOrderPage {
         driver.findElement(confirmOrderButton).click();
     }
 
-    //проверка большой кнопки в body
-    public void checkBigOrderButton(){
-        WebElement element = driver.findElement(bigMakeOrderButton);
-        ((JavascriptExecutor)driver).executeScript("arguments[0].scrollIntoView();", element);
-        new WebDriverWait(driver, Duration.ofSeconds(5))
-                .until(ExpectedConditions.elementToBeClickable(bigMakeOrderButton));
-        driver.findElement(bigMakeOrderButton).click();
-        String titleOfTheForm = driver.findElement(By.xpath("//div[@class= 'Order_Header__BZXOb']")).getText();
-        String expected = "Для кого самокат";
-        Assert.assertEquals("Окно не открылось", expected, titleOfTheForm);
-
-
+    public String getConfirmOrderText() {
+        return driver.findElement(confirmOrderText).getText();
     }
+
+
 }
 
 
